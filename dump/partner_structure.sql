024ac383d7cce63f2ef00999063090740f94adc7
Дмитрий <some.nugget@gmail.com>
2015-08-25 09:11:19 +0300

    Success.php. Структура БД для установки

--- dump/partner_structure.sql
+++ dump/partner_structure.sql
@@ -1,10 +1,10 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
+-- version 4.0.10.6
 -- http://www.phpmyadmin.net
 --
--- Хост: 127.0.0.1
--- Время создания: Авг 24 2015 г., 09:43
--- Версия сервера: 5.6.21
+-- Хост: 127.0.0.1:3306
+-- Время создания: Авг 24 2015 г., 11:26
+-- Версия сервера: 5.5.41-log
 -- Версия PHP: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -26,32 +26,35 @@ SET time_zone = "+00:00";
 -- Структура таблицы `landings`
 --
 
+DROP TABLE IF EXISTS `landings`;
 CREATE TABLE IF NOT EXISTS `landings` (
-`land_id` int(11) NOT NULL,
+  `land_id` int(11) NOT NULL AUTO_INCREMENT,
   `link` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `vip` varchar(250) NOT NULL,
   `extended` varchar(250) NOT NULL,
   `standard` varchar(250) NOT NULL,
-  `use_click_pay` tinyint(1) NOT NULL,
-  `use_fixed_pay` tinyint(1) NOT NULL,
-  `click_pay` int(11) NOT NULL,
-  `fixed_pay` int(11) NOT NULL,
-  `sort_order` varchar(15) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  `click_pay` varchar(255) NOT NULL,
+  `sort_order` varchar(15) NOT NULL,
+  PRIMARY KEY (`land_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
+-- --------------------------------------------------------
 
 --
 -- Структура таблицы `news`
 --
 
+DROP TABLE IF EXISTS `news`;
 CREATE TABLE IF NOT EXISTS `news` (
-`news_id` int(11) NOT NULL,
+  `news_id` int(11) NOT NULL AUTO_INCREMENT,
   `header` varchar(255) NOT NULL,
   `text` varchar(4095) NOT NULL,
   `land_id` int(15) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`news_id`),
+  UNIQUE KEY `news_id` (`news_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=22 ;
 
 -- --------------------------------------------------------
 
@@ -59,11 +62,13 @@ CREATE TABLE IF NOT EXISTS `news` (
 -- Структура таблицы `news_views`
 --
 
+DROP TABLE IF EXISTS `news_views`;
 CREATE TABLE IF NOT EXISTS `news_views` (
-`news_views_id` int(11) NOT NULL,
+  `news_views_id` int(11) NOT NULL AUTO_INCREMENT,
   `news_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`news_views_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
 
 -- --------------------------------------------------------
 
@@ -71,16 +76,19 @@ CREATE TABLE IF NOT EXISTS `news_views` (
 -- Структура таблицы `notifications`
 --
 
+DROP TABLE IF EXISTS `notifications`;
 CREATE TABLE IF NOT EXISTS `notifications` (
-`notification_id` int(11) NOT NULL,
+  `notification_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `theme` int(2) NOT NULL,
   `text` varchar(4095) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `is_new` tinyint(1) NOT NULL,
-  `stated_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
+  `stated_id` int(11) NOT NULL,
+  PRIMARY KEY (`notification_id`),
+  KEY `user_id` (`user_id`),
+  KEY `stated_id` (`stated_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=58 ;
 
 -- --------------------------------------------------------
 
@@ -88,27 +96,33 @@ CREATE TABLE IF NOT EXISTS `notifications` (
 -- Структура таблицы `profit`
 --
 
+DROP TABLE IF EXISTS `profit`;
 CREATE TABLE IF NOT EXISTS `profit` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `profit` decimal(8,0) DEFAULT '0',
-  `full_profit` decimal(10,0) DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  `full_profit` decimal(10,0) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=110 ;
+
+-- --------------------------------------------------------
 
 --
 -- Структура таблицы `promobanns`
 --
 
+DROP TABLE IF EXISTS `promobanns`;
 CREATE TABLE IF NOT EXISTS `promobanns` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci DEFAULT NULL,
-  `name` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `image` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
   `width` int(11) NOT NULL,
   `height` int(11) NOT NULL,
-  `code` text NOT NULL,
-  `land_id` int(15) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  `code` text CHARACTER SET latin1 NOT NULL,
+  `land_id` int(15) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=48 ;
 
 -- --------------------------------------------------------
 
@@ -116,13 +130,14 @@ CREATE TABLE IF NOT EXISTS `promobanns` (
 -- Структура таблицы `promovideo`
 --
 
+DROP TABLE IF EXISTS `promovideo`;
 CREATE TABLE IF NOT EXISTS `promovideo` (
-`id` int(11) NOT NULL,
-  `link` varchar(4096) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `link` varchar(4096) CHARACTER SET latin1 NOT NULL,
   `land_id` int(11) NOT NULL,
-  `banner_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
+  `banner_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=16 ;
 
 -- --------------------------------------------------------
 
@@ -130,8 +145,9 @@ CREATE TABLE IF NOT EXISTS `promovideo` (
 -- Структура таблицы `referrals`
 --
 
+DROP TABLE IF EXISTS `referrals`;
 CREATE TABLE IF NOT EXISTS `referrals` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(150) NOT NULL,
   `site` varchar(150) DEFAULT NULL,
   `region` varchar(150) DEFAULT NULL,
@@ -146,8 +162,9 @@ CREATE TABLE IF NOT EXISTS `referrals` (
   `recreate_interval` varchar(255) NOT NULL,
   `recreate_date` varchar(255) NOT NULL,
   `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `promo` varchar(150) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  `promo` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=223 ;
 
 -- --------------------------------------------------------
 
@@ -155,14 +172,16 @@ CREATE TABLE IF NOT EXISTS `referrals` (
 -- Структура таблицы `requests`
 --
 
+DROP TABLE IF EXISTS `requests`;
 CREATE TABLE IF NOT EXISTS `requests` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `ip` varchar(255) NOT NULL,
   `date` varchar(255) NOT NULL DEFAULT '',
   `click_pay` tinyint(1) NOT NULL,
   `land_id` int(255) NOT NULL,
-  `partner_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  `partner_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15477 ;
 
 -- --------------------------------------------------------
 
@@ -170,22 +189,21 @@ CREATE TABLE IF NOT EXISTS `requests` (
 -- Структура таблицы `settings`
 --
 
+DROP TABLE IF EXISTS `settings`;
 CREATE TABLE IF NOT EXISTS `settings` (
-`setting_id` int(11) NOT NULL,
+  `setting_id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(255) NOT NULL,
   `name` varchar(128) NOT NULL,
   `status` varchar(128) NOT NULL,
   `value` varchar(4095) NOT NULL,
-  `header` varchar(128) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+  `header` varchar(128) NOT NULL,
+  PRIMARY KEY (`setting_id`),
+  UNIQUE KEY `setting_id` (`setting_id`,`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=27 ;
 
---
--- Дамп данных таблицы `settings`
---
 
 INSERT INTO `settings` (`setting_id`, `type`, `name`, `status`, `value`, `header`) VALUES
 (1, '', 'click_pay', '1', '4', 'Цена за переход'),
-(2, '', 'exception_sites', '0', 'http://vk.com https://www.google.com.ua http://www.yandex.ua https://www.google.com.ru http://www.yandex.ru http://www.rambler.ru/', 'Запрещенные к закреплению сайты'),
 (11, 'pay_service', 'qiwi', '1', '', 'QIWI'),
 (12, 'pay_service', 'webmoney', '1', '', 'WebMoney'),
 (13, 'pay_service', 'yandex_money', '0', '', 'Яндекс Деньги'),
@@ -194,10 +212,15 @@ INSERT INTO `settings` (`setting_id`, `type`, `name`, `status`, `value`, `header
 (16, '', 'vip', '0', '20', 'VIP'),
 (17, '', 'extended', '0', '17', 'Расширенный'),
 (18, '', 'standard', '0', '15', 'Cтандартный'),
-(19, '', 'vk', '0', 'http://vk.com/', 'ВК'),
-(20, '', 'email', '0', 'somemail', 'Почта'),
-(21, '', 'skype', '0', 'someskype', 'Скайп'),
-(23, '', 'phone', '0', 'somephone', 'Номер телефона');
+(19, 'contacts', 'vk', '1', 'http://vk.com', 'ВК'),
+(20, 'contacts', 'email', '1', 'somemail@mail.com', 'Почта'),
+(21, 'contacts', 'skype', '1', 'someskype', 'Скайп'),
+(22, '', 'landing_link', '1', 'somelink', 'Ссылка'),
+(23, 'land', 'main_header', '1', 'ПАРТНЕРСКАЯ ПРОГРАММА\r\nПАВЛУЦКОГО АЛЕКСАНДРА', 'Основной заголовок'),
+(24, 'land', 'main_bg', '1', '/img/main_bg.jpg', 'Главный фон'),
+(25, 'land', 'main_bg_repeat', '1', 'no-repeat', 'Повторение главного фона'),
+(26, 'contacts', 'phone', '1', 'somephone', 'Телефон');
+
 
 -- --------------------------------------------------------
 
@@ -205,16 +228,18 @@ INSERT INTO `settings` (`setting_id`, `type`, `name`, `status`, `value`, `header
 -- Структура таблицы `stateds`
 --
 
+DROP TABLE IF EXISTS `stateds`;
 CREATE TABLE IF NOT EXISTS `stateds` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `money` decimal(8,0) NOT NULL,
   `status` varchar(50) DEFAULT 'Ожидает оплату',
   `pay_type` varchar(50) NOT NULL,
   `requisites` varchar(50) NOT NULL,
   `description` text,
-  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=24103 ;
 
 -- --------------------------------------------------------
 
@@ -222,8 +247,9 @@ CREATE TABLE IF NOT EXISTS `stateds` (
 -- Структура таблицы `user`
 --
 
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `role` varchar(50) DEFAULT 'user',
   `username` varchar(150) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
@@ -243,12 +269,13 @@ CREATE TABLE IF NOT EXISTS `user` (
   `skype` varchar(128) NOT NULL,
   `use_click_pay` tinyint(1) NOT NULL,
   `click_pay` int(11) NOT NULL,
-  `use_fixed_pay` tinyint(1) NOT NULL,
-  `fixed_pay` varchar(11) NOT NULL,
+  `use_fixed_pay` int(1) NOT NULL,
+  `fixed_pay` int(11) NOT NULL,
   `promo_code` varchar(128) NOT NULL,
   `unc_site` varchar(255) NOT NULL,
-  `land_id` int(11) NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  `land_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=190 ;
 
 -- --------------------------------------------------------
 
@@ -256,11 +283,13 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Структура таблицы `users_landings`
 --
 
+DROP TABLE IF EXISTS `users_landings`;
 CREATE TABLE IF NOT EXISTS `users_landings` (
-`users_landings_id` int(11) NOT NULL,
+  `users_landings_id` int(11) NOT NULL AUTO_INCREMENT,
   `land_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`users_landings_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
 
 -- --------------------------------------------------------
 
@@ -268,92 +297,14 @@ CREATE TABLE IF NOT EXISTS `users_landings` (
 -- Структура таблицы `versions`
 --
 
+DROP TABLE IF EXISTS `versions`;
 CREATE TABLE IF NOT EXISTS `versions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `version` varchar(50) NOT NULL,
+  `version` varchar(50) CHARACTER SET latin1 NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
 
-
---
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `landings`
---
-ALTER TABLE `landings`
- ADD PRIMARY KEY (`land_id`);
-
---
--- Индексы таблицы `news`
---
-ALTER TABLE `news`
- ADD PRIMARY KEY (`news_id`), ADD UNIQUE KEY `news_id` (`news_id`);
-
---
--- Индексы таблицы `news_views`
---
-ALTER TABLE `news_views`
- ADD PRIMARY KEY (`news_views_id`);
-
---
--- Индексы таблицы `notifications`
---
-ALTER TABLE `notifications`
- ADD PRIMARY KEY (`notification_id`), ADD KEY `user_id` (`user_id`), ADD KEY `stated_id` (`stated_id`);
-
---
--- Индексы таблицы `profit`
---
-ALTER TABLE `profit`
- ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `promobanns`
---
-ALTER TABLE `promobanns`
- ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `promovideo`
---
-ALTER TABLE `promovideo`
- ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `referrals`
---
-ALTER TABLE `referrals`
- ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `requests`
---
-ALTER TABLE `requests`
- ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `settings`
---
-ALTER TABLE `settings`
- ADD PRIMARY KEY (`setting_id`), ADD UNIQUE KEY `setting_id` (`setting_id`,`name`);
-
---
--- Индексы таблицы `stateds`
---
-ALTER TABLE `stateds`
- ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `user`
---
-ALTER TABLE `user`
- ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `users_landings`
---
-ALTER TABLE `users_landings`
- ADD PRIMARY KEY (`users_landings_id`);
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- success.php
+++ success.php
@@ -4,7 +4,54 @@
     <title>Активация</title>
 </head>
 <body>
-        Код лицензии успешно подтверждён. </br>
-        <a href="/">Перейти в админку</a>
+<style type="text/css">
+@import url(http://fonts.googleapis.com/css?family=Open+Sans:400,400,500,600,700&subset=latin,cyrillic-ext,cyrillic);
+* {
+	box-sizing: border-box;
+	color: #3a4453;
+	font-family: 'Open Sans';
+}
+html {
+	background: #F0F3F7;
+	height: 100%;
+	min-height: 100%;
+}
+body {
+	background: #F0F3F7;
+	height: 100%;
+	min-height: 100%;
+}
+.block {
+	background-color: #FFF;
+	box-shadow: 0px 0px 6px rgba(139, 157, 175, 0.1);
+	padding: 30px;
+	width: 90%;
+	margin: 0 auto;
+}
+h5 {
+	color: #697887;
+	font: 600 19px 'Open Sans';
+	margin: 0 0 15px 0;
+	overflow: hidden;
+	text-align: left;
+}
+.block section {
+	margin-bottom: 15px;
+}
+.block section:last-child {
+	margin-bottom: 0;
+}
+
+</style>
+
+
+	<div class="block">
+        <h5>
+			Код лицензии успешно подтверждён.
+        </h5>
+        <p>
+        	<a href="/">Перейти на главную</a>
+        </p>
+	</div>
 </body>
 </html>
